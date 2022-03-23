6b3a3ee212215dca483c63d138b21c7ee5a0f2e4
Lucas Clement <2745633c@student.gla.ac.uk>
2022-03-23 18:09:21 +0000

    Update population script

--- populate_cycle_angelo.py
+++ populate_cycle_angelo.py
@@ -5,44 +5,44 @@ import django
 django.setup()
 
 from cycle_angelo.models import User, Post, Comment
+import random
 
 def populate():
-    
+
     post1_comments = [{'content' : 'Nice ride!'}, {'content' : 'Wow that was fast!'}]
     post2_comments = [{'content' : 'How much did that cost?!'}, {'content' : 'Ready to race you soon!'}]
     post3_comments = [{'content' : 'If you add intervals to training that might help'}, {'content' : 'Make sure to sleep enough'}]
-    
+
     test_blogPosts = {'What a day to go out in the bike! 80k\'s in the bank.' : {'comments':post1_comments},
                        'New bike day, ready for some miles in the summer!' : {'comments':post2_comments},
                        'Wanting to stay motivated and get better on the bike. Any ideas?' : {'comments':post3_comments}}
-                       
-    
+
+
     for blogPost, comments in test_blogPosts.items():
         bp = add_post(blogPost)
         for c in comments['comments']:
             add_comment(bp, c['content'])
-    
-    
+
+
     for bp in Post.objects.all():
         for c in Comment.objects.filter(post=bp):
             print(f'- {bp} : {c}')
-    
-    
-def add_post(bp):
 
+
+def add_post(bp):
+    creator = User(username=random.randint(1,100))
+    creator.save()
     content = bp
-    bp = Post.objects.get_or_create(content=content)[0]
+    bp = Post.objects.get_or_create(content=content, creator=creator)[0]
     bp.save()
     return bp
-    
+
 def add_comment(bp, content):
     c = Comment.objects.get_or_create(post=bp, content=content)[0]
     c.save()
     return c
-    
-    
+
+
 if __name__ == '__main__':
     print('Starting Cycle Angelo population script...')
     populate()
-    
-    
