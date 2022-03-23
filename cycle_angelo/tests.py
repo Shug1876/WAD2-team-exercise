dc7570c2f0f769927215c76fa257cc72f3f4f8a9
Lucas Clement <2745633c@student.gla.ac.uk>
2022-03-23 17:13:26 +0000

    New tests added + contact page update

--- cycle_angelo/tests.py
+++ cycle_angelo/tests.py
@@ -1,11 +1,23 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WAD2_team_exercise.settings')
+
+import django
+django.setup()
+
 from django.test import TestCase
-from cycle_angelo.models import Post
+from cycle_angelo.models import Post, User
 from django.urls import reverse
 
 # Create your tests here.
 
 
 class IndexViewTests(TestCase):
+    '''
+    test if displays proper posts
+    posts with most comments
+    recent posts
+    hyperlinks work
+    '''
     def test_index_view_with_no_posts(self):
         """
         If no posts exist, the page should just be empty.
@@ -21,21 +33,76 @@ class IndexViewTests(TestCase):
         """
         Checks whether posts are displayed correctly when present
         """
-        add_post('I love bikes', 'Bikes are great')
-        add_post('Great ride this morning', "Lovely ride along the Kelvin")
+        add_post('I love bikes', 'Bikes are great', 'Lucas')
+        add_post('Great ride this morning', "Lovely ride along the Kelvin", 'Adam')
 
         response = self.client.get(reverse('cycle_angelo:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "I love bikes")
-        self.assertContains(repsonse, "Great ride this morning")
+        self.assertContains(response, "Great ride this morning")
 
-        num_posts = len(repsonse.context['posts'])
+        num_posts = len(response.context['new_posts'])
         self.assertEquals(num_posts, 2)
 
-def add_post(title, content, likes=0):
-    post = Post.objects.get_or_create(title=title)
-    post.content = content
-    post.likes = likes
+    def test_top_five_posts(self):
+        '''
+        Checks to make sure the top posts are ordered correctly by number of
+        comments
+        '''
+        add_post('I love bikes', 'Bikes are great', 'Lucas', 10)
+        add_post('Morning!', 'Lovely ride along the Kelvin', 'Adam', 2)
+        add_post('Biking', 'good places to ride?', 'Nile', 5)
+        add_post('Gears', 'need new gears', 'Hamish', 1)
+        add_post('Howdy!', 'Wroong app!', 'Rango', 7)
 
+        rankings = ['<Post: I love bikes>', '<Post: Howdy!>', '<Post: Biking>',
+                    '<Post: Morning!>', '<Post: Gears>']
+
+        response = self.client.get(reverse('cycle_angelo:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['top_posts'], rankings)
+
+    def test_five_new_posts(self):
+        '''
+        Checks to make sure the order of the new posts are being shown correctly
+        '''
+        add_post('I love bikes', 'Bikes are great', 'Lucas', 10)
+        add_post('Morning!', 'Lovely ride along the Kelvin', 'Adam', 2)
+        add_post('Biking', 'good places to ride?', 'Nile', 5)
+        add_post('Gears', 'need new gears', 'Hamish', 1)
+        add_post('Howdy!', 'Wroong app!', 'Rango', 7)
+
+        rankings = ['<Post: Howdy!>', '<Post: Gears>', '<Post: Biking>',
+                    '<Post: Morning!>', '<Post: I love bikes>']
+
+        response = self.client.get(reverse('cycle_angelo:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['new_posts'], rankings)
+
+
+def add_post(title, content, name, comments=0):
+    creator = User(username=name)
+    creator.save()
+    post = Post.objects.get_or_create(title=title, content=content,
+                                    creator=creator, number_of_comments=comments)[0]
     post.save()
     return post
+
+
+class AddPostViewTests(TestCase):
+    '''
+    test to make sure Posts can be added properly
+    check for with and without title
+    check content is present
+    if photos work
+    slug?
+    '''
+
+    pass
+
+class AddCommentViewTests(TestCase):
+    '''
+    tests to see if adding comments works
+    empty comments
+
+    '''
